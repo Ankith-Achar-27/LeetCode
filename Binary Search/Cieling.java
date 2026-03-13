97c1337c979e7f11b863727860096aac9192a05f
Ankith H S <ankith270600@gmail.com>
2026-03-13 20:08:13 +0530

    Returning the cieling of target
    If the target is not found then return the smallest element in an array greater than the target

---
+++ Binary Search/Cieling.java
@@ -0,0 +1,25 @@
+public class Cieling {
+    public static void main(String[] args) {
+        int[] nums = {2,3,5,9,14,16,18,19};
+        int target = 4;
+        int ans = ciel(nums,target);
+        System.out.println(nums[ans]);
+    }
+    static int ciel(int[] nums,int tar){
+        int start=0;
+        int end = nums.length-1;
+
+        while (start<=end){
+            int mid = start+(end-start)/2;
+            if(tar<nums[mid]){
+                end=mid-1;
+            }
+            else if(tar>nums[mid]){
+                start=mid+1;
+            }
+            else if(tar==nums[mid])
+                return mid;
+        }
+        return start;
+    }
+}
