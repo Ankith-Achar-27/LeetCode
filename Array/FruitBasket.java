051c9ba20414f39142cff989337bc9fbc968ef42
Ankith H S <ankith270600@gmail.com>
2026-03-19 23:09:29 +0530

    3447.Fruits Into Baskets II

---
+++ Array/FruitBasket.java
@@ -0,0 +1,22 @@
+public class FruitBasket {
+    public static void main(String[] args) {
+        int[] fruits={3,6,1};
+        int[] baskets={6,4,7};
+        FruitBasket obj = new FruitBasket();
+        System.out.println(obj.numOfUnplacedFruits(fruits,baskets));
+
+    }
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int count=0;
+        for(int i=0;i<fruits.length;i++){
+            for (int j=0;j<baskets.length;j++){
+                if (fruits[i]<=baskets[j]){
+                    count++;
+                    baskets[j]=Integer.MIN_VALUE;
+                    break;
+                }
+            }
+        }
+        return fruits.length-count;
+    }
+}
