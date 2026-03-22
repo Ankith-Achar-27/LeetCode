b1a52d1647ea1c6333a4f533b96b5299388d642d
Ankith H S <ankith270600@gmail.com>
2026-03-22 23:45:52 +0530

    Split Array Largest Sum

---
+++ Binary Search/Hard_SplitArrayLargestSum.java
@@ -0,0 +1,42 @@
+public class Hard_SplitArrayLargestSum {
+    public static void main(String[] args) {
+        int[] nums={7,2,5,10,8};
+        int k=2;
+        Hard_SplitArrayLargestSum obj = new Hard_SplitArrayLargestSum();
+        System.out.println(obj.splitArray(nums,k));
+    }
+    public int splitArray(int[] nums, int k) {
+        int start=0;
+        int end=0;
+        for(int num:nums){
+            start=Math.max(start,num);
+            end+=num;
+        }
+        while (start < end) {
+            // try for the middle as potential ans
+            int mid = start + (end - start) / 2;
+
+            // calculate how many pieces you can divide this in with this max sum
+            int sum = 0;
+            int pieces = 1;
+            for(int num : nums) {
+                if (sum + num > mid) {
+                    // you cannot add this in this subarray, make new one
+                    // say you add this num in new subarray, then sum = num
+                    sum = num;
+                    pieces++;
+                } else {
+                    sum += num;
+                }
+            }
+
+            if (pieces > k) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+
+        }
+        return end; // here start == end
+    }
+}
