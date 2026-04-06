8df9708da7e86a7f90d203b1f00b158550685ad1
Ankith H S <ankith270600@gmail.com>
2026-04-06 17:57:49 +0530

    Reverse String

---
+++ String/Easy/ReverseString_344.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+class TestCase{
+    char[] str;
+    TestCase(char[] s){
+        this.str=s;
+    }
+}
+
+public class ReverseString_344 {
+    public static void main(String[] args) {
+        TestCase test1 = new TestCase(new char[]{'h', 'e', 'l', 'l', 'o'});
+        TestCase test2 = new TestCase(new char[]{'H', 'a', 'n', 'n', 'a','h'});
+
+        reverseString(test1.str);
+        System.out.println("Test Case1 Solution: "+ Arrays.toString(test1.str));
+        reverseString(test2.str);
+        System.out.println("Test Case2 Solution: "+ Arrays.toString(test2.str));
+
+
+    }
+    static void reverseString(char[] s) {
+        int start=0;
+        int end=s.length-1;
+        while (start<=end){
+            swap(s,start,end);
+            start++;
+            end--;
+        }
+
+    }
+    static char[] swap(char[] s,int n1,int n2){
+        char temp = s[n1];
+        s[n1]=s[n2];
+        s[n2]=temp;
+        return s;
+    }
+}
