ae3b68f0d08cdd1f98b341a6083cf0ad38358fd0
Ankith H S <ankith270600@gmail.com>
2026-06-30 21:30:18 +0530

    First Missing Positive

--- Arrays/Hard/FirstMissingPositive_41.java
+++ Arrays/Hard/FirstMissingPositive_41.java
@@ -9,35 +9,37 @@ class TestCase_41{
 }
 public class FirstMissingPositive_41 {
     public static void main(String[] args) {
-        TestCase_41 test1 = new TestCase_41(new int[]{1,2,0});
-        TestCase_41 test2 = new TestCase_41(new int[]{3,4,-1,1});
-        TestCase_41 test3 = new TestCase_41(new int[]{7,8,9,11,12});
+        TestCase_41 test1 = new TestCase_41(new int[]{1, 2, 0});
+        TestCase_41 test2 = new TestCase_41(new int[]{3, 4, -1, 1});
+        TestCase_41 test3 = new TestCase_41(new int[]{7, 8, 9, 11, 12});
 
-        System.out.println("Case1 Solution: "+firstMissingPositive(test1.nums));
-        System.out.println("Case2 Solution: "+firstMissingPositive(test2.nums));
-        System.out.println("Case3 Solution: "+firstMissingPositive(test3.nums));
+        System.out.println("Case1 Solution: " + firstMissingPositive(test1.nums));
+        System.out.println("Case2 Solution: " + firstMissingPositive(test2.nums));
+        System.out.println("Case3 Solution: " + firstMissingPositive(test3.nums));
     }
+
     public static int firstMissingPositive(int[] nums) {
-        int i=0;
-        while (i< nums.length){
-            int correct = nums[i]-1;
-            if(nums[i]>0 && nums[i]< nums.length && nums[i]!=nums[correct]){
-                swap(nums,i,correct);
-            }
-            else {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
                 i++;
             }
         }
-        for (int index=0;index< nums.length;index++){
-            if(nums[index]!=index+1){
-                return index+1;
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                return index + 1;
             }
         }
-        return nums.length+1;
+        return nums.length + 1;
     }
-    static void swap(int[] arr, int first, int second){
+
+    static void swap(int[] arr, int first, int second) {
         int temp = arr[first];
         arr[first] = arr[second];
         arr[second] = temp;
 
+    }
 }
