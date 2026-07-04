645a62eb42387f1700ef31b4e5206a77fe59d17b
Ankith H S <ankith270600@gmail.com>
2026-07-04 19:56:12 +0530

    Sort Array By Parity

---
+++ Arrays/Easy/SortByParity_905.java
@@ -0,0 +1,35 @@
+package Easy;
+
+import java.util.Arrays;
+
+class TestCase_41{
+    int [] nums;
+    TestCase_41(int[] nums){
+        this.nums=nums;
+    }
+}
+public class SortByParity_905 {
+    public static void main(String[] args) {
+        TestCase_41 test1 = new TestCase_41(new int[] {3,1,2,4});
+        TestCase_41 test2 = new TestCase_41(new int[]{0});
+
+        System.out.println("Test1 : "+Arrays.toString(sortArrayByParity(test1.nums)));
+        System.out.println("Test2 :"+Arrays.toString(sortArrayByParity(test2.nums)));
+
+    }
+    public static int[] sortArrayByParity(int[] nums) {
+        for (int i=0,j=0;j< nums.length;j++){
+            if ((nums[j]&1)==0){
+                swap(nums,i,j);
+                i++;
+            }
+        }
+        return nums;
+
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
