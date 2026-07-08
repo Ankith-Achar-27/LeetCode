0fbdc83f2dd795f2a9abc781831a16a23df1b4b4
Ankith H S <ankith270600@gmail.com>
2026-07-08 18:12:31 +0530

    updated syntax

--- Arrays/Easy/ContainsDuplicate_217.java
+++ Arrays/Easy/ContainsDuplicate_217.java
@@ -4,21 +4,23 @@ import java.util.Arrays;
 
 public class ContainsDuplicate_217 {
     public static void main(String[] args) {
-        int[] n1 = {1,2,3,1};
-        int[] n2 = {1,2,3,4};
-        int[] n3 = {1,1,1,3,3,4,3,2,4,2};
+        int[] n1 = {1, 2, 3, 1};
+        int[] n2 = {1, 2, 3, 4};
+        int[] n3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
         System.out.println(containsDuplicate(n1));
         System.out.println(containsDuplicate(n2));
         System.out.println(containsDuplicate(n3));
 
     }
+
     public static boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
 
-        for (int j=1;j< nums.length;j++){
-            if(nums[j]==nums[j-1]) {
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] == nums[j - 1]) {
                 return true;
             }
         }
         return false;
+    }
 }
